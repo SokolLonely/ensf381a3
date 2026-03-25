037ec3f3dbb016beb9a1afb10a971e88134ba181
sokolLonely <simonryab@gmail.com>
2026-03-25 14:49:46 -0600

    data files (not finished)

--- src/data/flavors.js
+++ src/data/flavors.js
@@ -8,5 +8,65 @@ duration: "8 weeks",
 image: "images/vanilla.jpg"
 },
 // Add 9 more flavors...
+{
+id: 2,
+name: "Chocolate Bliss",
+price: "$4.99",
+description: " Classic creamy chocolate ice cream made with fresh ingredients.",
+duration: "8 weeks",
+image: "images/vanilla.jpg"
+},
+{
+id: 3,
+name: "Strawberry Swirl",
+price: "$4.99",
+description: " Classic creamy strawberry ice cream made with fresh ingredients.",
+duration: "8 weeks",
+image: "images/vanilla.jpg"
+},
+{
+id: 4,
+name: "Cookies & Cream",
+price: "$4.99",
+description: " Classic creamy  ice cream  with cookies.",
+duration: "8 weeks",
+image: "images/vanilla.jpg"
+},
+{
+id: 5,
+name: "Mint Chocolate Chip",
+price: "$4.99",
+description: "smaller  Classic chocolate ice cream  with cookies.",
+duration: "8 weeks",
+image: "images/vanilla.jpg"
+},
+
+{
+id: 6,
+name: "Mango Tango",
+price: "$5.99",
+description: "smaller  Classic chocolate ice cream  with cookies.",
+duration: "8 weeks",
+image: "images/vanilla.jpg"
+},
+
+{
+id: 7,
+name: "Mint Chocolate Chip",
+price: "$7.99",
+description: "smaller  Classic chocolate ice cream  with cookies.",
+duration: "8 weeks",
+image: "images/vanilla.jpg"
+},
+
+{
+id: 8,
+name: "Mint Chocolate Chip",
+price: "$6.00",
+description: "smaller  Classic chocolate ice cream  with cookies.",
+duration: "8 weeks",
+image: "images/vanilla.jpg"
+},
+//add 2 more
 ];
 export default flavors;
--- src/data/reviews.js
+++ src/data/reviews.js
@@ -6,5 +6,24 @@ review: "The ice cream was delicious and very creamy!",
 rating: 5
 },
 // Add 3 more reviews...
+{
+customerName: "Jippy Morgan",
+flavorName: " Vanilla Dream",
+review: "better than in high school",
+rating: 3
+},
+{
+customerName: "Iraj",
+flavorName: " Vanilla Dream",
+review: "really good",
+rating: 4
+},
+{
+customerName: "Donald",
+flavorName: " Vanilla Dream",
+review: "Awful customer service",
+rating: 1
+},
+
 ];
 export default reviews;
