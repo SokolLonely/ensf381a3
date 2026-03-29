2bf880e4c9bf1124afa95a954f2ccf292b5ae69a
basema-k <basema.khan1@ucalgary.ca>
2026-03-29 13:48:53 -0600

    fix header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,19 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header>
-      <div className="flexbox_homepage_header">
-        <img width="50px" height="50px" src="/logo.jpeg" alt="Sweet Scoop Logo" style={{ borderRadius: '50%' }} />
-        <b>Sweet Scoop Ice Cream</b>
-      </div>
+    <>
+      <header>
+        <div className="flexbox_homepage_header">
+          <img width="50px" height="50px" src="/logo.jpeg" alt="Sweet Scoop Logo" />
+          <h2>Sweet Scoop Ice Cream Shop</h2>
+        </div>
+      </header>
       <div className="homepage_menu">
-        <Link to="/">Homepage</Link>
+        <Link to="/">Home</Link>
         <Link to="/flavors">Flavors</Link>
         <Link to="/login">Login</Link>
       </div>
-    </header>
+    </>
   );
 }
 
