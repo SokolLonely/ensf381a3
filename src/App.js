afd2b737d7d376ff42befb82f50809a4e03284dd
basema-k <basema.khan1@ucalgary.ca>
2026-03-29 13:28:30 -0600

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import FlavorsPage from './pages/FlavorsPage';
+import LoginPage from './pages/LoginPage';
 import './styles.css';
 
 function App() {
